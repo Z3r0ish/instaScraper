3b832a4198e78f607fc80728439a45dd7594377c
Z3r0ish <Z3r0ish@github.com>
2020-07-22 19:03:21 -0400

    added items to the config

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ accounts = ["acount1", "account2"]
 driver = "chromedriver"
 filepath = "/insta/media/"
 scrollTime = 5
+followerXpath = """//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span"""
+timeXpath = """//*[@id="react-root"]/section/main/div/div/article/div[3]/div[2]/a/time"""
+connentXpath = """//meta[@property="og:type"]"""
+likesXpath = """//*[@id="react-root"]/section/main/div/div/article/div[3]/section[2]/div/div/button"""
--- instaScraper.py
+++ instaScraper.py
@@ -11,83 +11,86 @@ null = 0
 
 # open chrome / open 
 driver = webdriver.Chrome(driver)
+try:
+    for account in accounts:
+        
+        # loads in the account page and scrolls down
+        driver.get('https://www.instagram.com/' + accounts[i] + '/')
+        
+        # gets the follower count
+        followers = driver.find_element_by_xpath(followerXpath).text
+        print(followers)
 
-for account in accounts:
-    
-    # loads in the account page and scrolls down
-    driver.get('https://www.instagram.com/' + accounts[i] + '/')
-    
-    # gets the follower count
-    followers = driver.find_element_by_xpath("""//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span""").text
-    print(followers)
-
-    # scrolling (does a max of 4 page heights)
-    lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-    match=False
-    scroll=0
-    while(match==False):
-        lastCount = lenOfPage
-        time.sleep(scrollTime)
+        # scrolling (does a max of 4 page heights)
         lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-        if lastCount==lenOfPage:
-            match=True
-        if scroll > 4:
-            match=True
-        scroll += 1
+        match=False
+        scroll=0
+        while(match==False):
+            lastCount = lenOfPage
+            time.sleep(scrollTime)
+            lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+            if lastCount==lenOfPage:
+                match=True
+            if scroll > 4:
+                match=True
+            scroll += 1
 
-    # gets all post
-    posts = []
-    links = driver.find_elements_by_tag_name('a')
-    for link in links:
-        p = 0
-        post = link.get_attribute('href')
-        if '/p/' in post:
-            posts.append(post)
-            p += 1
-            print(post)
+        # gets all post
+        posts = []
+        links = driver.find_elements_by_tag_name('a')
+        for link in links:
+            p = 0
+            post = link.get_attribute('href')
+            if '/p/' in post:
+                posts.append(post)
+                p += 1
+                print(post)
 
-    # spliting up the url, finding out if it's an image or video and downloading it
-    download_url = ' '
-    for post in posts:
-        driver.get(post) # loads in the post
-        shortcode = driver.current_url.split('/')[-2] # gets the url code
+        # spliting up the url, finding out if it's an image or video and downloading it
+        download_url = ' '
+        for post in posts:
+            driver.get(post) # loads in the post
+            shortcode = driver.current_url.split('/')[-2] # gets the url code
 
-        # get the time it was created
-        datetime = driver.find_element_by_xpath("""//*[@id="react-root"]/section/main/div/div/article/div[3]/div[2]/a/time""").get_attribute('datetime')
-        print(datetime)
-        
-        # find out if it's a video or immage
-        type = driver.find_element_by_xpath('//meta[@property="og:type"]').get_attribute('content')
+            # get the time it was created
+            datetime = driver.find_element_by_xpath(timeXpath).get_attribute('datetime')
+            print(datetime)
+            
+            # find out if it's a video or immage
+            type = driver.find_element_by_xpath(connentXpath).get_attribute('content')
 
-        # create file name
-        name = [filepath, accounts[i], '&', followers, '&', datetime]
-        nameJoined = ''.join(name)
-        
-        # gets the numbers of likes
-        try:
-            likes = driver.find_element_by_xpath("""//*[@id="react-root"]/section/main/div/div/article/div[3]/section[2]/div/div/button""").text
-            print(likes)
-            likesSplit = likes.split(' ')
-        # if it couldn't get the likes
-        except:
-            likesSplit = ['null', 'null'] 
-            print('Could not get the likes')
-            null += 1
+            # create file name
+            name = [filepath, accounts[i], '&', followers, '&', datetime]
+            nameJoined = ''.join(name)
+            
+            # gets the numbers of likes
+            try:
+                likes = driver.find_element_by_xpath(likesXpath).text
+                print(likes)
+                likesSplit = likes.split(' ')
+            # if it couldn't get the likes
+            except:
+                likesSplit = ['null', 'null'] 
+                print('Could not get the likes')
+                null += 1
 
-        # downloads it
-        if likesSplit[0] != 'null':
-            if type == 'video':
-                download_url = driver.find_element_by_xpath('//meta[@property="og:video"]').get_attribute('content')
-                urllib.request.urlretrieve(download_url, nameJoined + '&' + likesSplit[0] + '&' + "{}.mp4".format(shortcode))
-            else:
-                download_url = driver.find_element_by_xpath('//meta[@property="og:image"]').get_attribute('content')
-                urllib.request.urlretrieve(download_url, nameJoined + '&' + likesSplit[0] + '&' + "{}.jpg".format(shortcode))
-        print(type + ' ' + download_url)
-        numberPost += 1
-    i += 1
+            # downloads it
+            if likesSplit[0] != 'null':
+                if type == 'video':
+                    download_url = driver.find_element_by_xpath('//meta[@property="og:video"]').get_attribute('content')
+                    urllib.request.urlretrieve(download_url, nameJoined + '&' + likesSplit[0] + '&' + "{}.mp4".format(shortcode))
+                else:
+                    download_url = driver.find_element_by_xpath('//meta[@property="og:image"]').get_attribute('content')
+                    urllib.request.urlretrieve(download_url, nameJoined + '&' + likesSplit[0] + '&' + "{}.jpg".format(shortcode))
+            print(type + ' ' + download_url)
+            numberPost += 1
+        i += 1
 
-driver.quit()
+    driver.quit()
 
-print('instaScraper has finished')
-print(str(numberPost) + ' post where scraped')
-print(str(null) + ' have no likes')
+    print('instaScraper has finished')
+    print(str(numberPost) + ' post where scraped')
+    print(str(null) + ' have no likes')
+
+except:
+    print('There was a problem while trying to run the script')
